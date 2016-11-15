b6778920f7c48909fef9532fa2e768e1900716d6
Klarhub <klarobskuro@mailoo.org>
2016-11-15 19:26:32 +0100

    Add Bands.java

---
+++ CDI-Festival/src/fr/cdiFestival/service/Bands.java
@@ -0,0 +1,21 @@
+package fr.cdiFestival.service;
+
+import java.util.ArrayList;
+
+import fr.cdiFestival.model.Band;
+
+/**
+ * ArrayList's subclass for Band.
+ * 
+ * @author Claire
+ * @version 20161115
+ */
+public class Bands extends ArrayList<Band> {
+
+	private static final long serialVersionUID = 1L;
+
+	public Bands() {
+		super();
+	}
+	
+}
