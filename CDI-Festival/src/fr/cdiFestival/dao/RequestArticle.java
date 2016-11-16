9cf87790f67f5ef1b4c305627337f913f2e9b9e9
Jonathan Fuentes <foujo@hotmail.com>
2016-11-16 14:54:40 +0100

    Creation : Article, Articles, ConnectionBdd, RequestArticle

---
+++ CDI-Festival/src/fr/cdiFestival/dao/RequestArticle.java
@@ -0,0 +1,157 @@
+package fr.cdiFestival.dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import fr.cdiFestival.model.Article;
+import fr.cdiFestival.service.Articles;
+
+public class RequestArticle {
+
+	private ConnectionBdd		connection;
+	private	PreparedStatement 	prepStmt;
+	private ResultSet 			result;
+	
+	private int			id;
+	private String		author;
+	private String		date;
+	private	String		title; 
+	private String		content;
+	private Article		article;
+	private Articles	listArticle;
+	
+	//Constructeur
+	public RequestArticle() {
+		connection= new ConnectionBdd();
+	}
+	
+	//Méthodes DLM
+	public void add(int id, String author, String date, String title, String content) {
+		
+		try {
+			connection.initConnectionLocal();
+			
+			prepStmt = connection.prepareStatement("insert into article VALUES (?, ?, ?, ?, ?)");
+			prepStmt.setInt(1,id); 
+			prepStmt.setString(2, author); 
+			prepStmt.setString(3, date);
+			prepStmt.setString(4,title); 
+			prepStmt.setString(5, content);  
+			prepStmt.executeUpdate();
+			
+			if (prepStmt != null)	prepStmt.close();
+			if (connection != null)	connection.closeConnection();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public void delete(int id) {
+		try {
+			connection.initConnectionLocal();
+			
+			prepStmt = connection.prepareStatement("delete article where id = ?");
+			prepStmt.setInt(1, id);
+			prepStmt.executeUpdate();	
+			
+			if (prepStmt != null)	prepStmt.close();
+			if (connection != null)	connection.closeConnection();
+		} 
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public Article getArticle(int id) {
+		article	= null;
+		
+		try {
+			connection.initConnectionLocal();
+			
+			prepStmt = connection.prepareStatement("select id, author, date, title, content from article where id = ?");
+			prepStmt.setInt(1,id); 
+			result = prepStmt.executeQuery();
+
+			while (result.next()) {
+				author		= result.getString("author");
+				date		= result.getString("date");
+				title		= result.getString("title");
+				content		= result.getString("content");
+			}
+
+				if ((id != 0) && (author != null) && (date != null) && (title != null) && (content != null)) {	
+					article	= new Article(id, author, date, title, content);
+				}
+				
+
+			if (prepStmt != null)	prepStmt.close();
+			if (connection != null)	connection.closeConnection();
+		} 
+		catch (SQLException e) {
+			e.printStackTrace();
+		}	
+		return article;
+	}	
+
+
+	public Articles getArticles() {
+		listArticle = new Articles();
+		article 	= null;
+
+		try {
+			connection.initConnectionLocal();
+
+			prepStmt = connection.prepareStatement("select id, author, date, title, content from article");
+
+			result = prepStmt.executeQuery();
+
+			while (result.next()) {
+				id			= result.getInt("id");
+				author		= result.getString("author");
+				date		= result.getString("date");
+				title		= result.getString("title");
+				content		= result.getString("content");
+
+				if ((id != 0) && (author != null) && (date != null) && (title != null) && (content != null)) {	
+					article	= new Article(id, author, date, title, content);
+				}
+
+				listArticle.add(article);
+			}			
+
+			if (prepStmt != null)	prepStmt.close();
+			if (connection != null)	connection.closeConnection();
+
+		} 
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return listArticle;
+	}
+	
+	public void upDateArticle (int id, String author, String date, String title, String content) {
+
+		try {
+			connection.initConnectionLocal();
+
+			prepStmt = connection.prepareStatement("UPDATE article set author = ?, date = ?, title = ?, content = ? where id = ?"); 
+			prepStmt.setString(1, author); 
+			prepStmt.setString(2, date);
+			prepStmt.setString(3,title); 
+			prepStmt.setString(4, content);
+			prepStmt.setInt(5,id);
+			prepStmt.executeUpdate();
+
+			if (prepStmt != null)	prepStmt.close();
+			if (connection != null)	connection.closeConnection();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
