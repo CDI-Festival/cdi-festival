a59fcc3893f986c7a92733ad2397101bb43bd5d6
Nicolas Tarral <nicolas.tarral@gmail.com>
2016-11-23 09:53:30 +0100

    Ajout javadoc sur class Pass.

--- CDI-Festival/src/fr/cdiFestival/model/Pass.java
+++ CDI-Festival/src/fr/cdiFestival/model/Pass.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 /**
- * class to create differents type 
+ * class to create differents pass type 
  * type : 
  * 0 day 1
  * 1 day 2
@@ -15,7 +15,8 @@ import java.util.ArrayList;
  * 5 days 2-3
  * 6 days 1-2-3
  * 
- * @author Administrateur
+ * @author Nicolas Tarral
+ * @version : 2016-11-23
  *
  */
 public class Pass {
@@ -34,7 +35,14 @@ public class Pass {
 
 	
 	
-	
+	/**
+	 * Constructeur secondaire 
+	 * @param type identifie le pass
+	 * @param nombre nombre de pass restant
+	 * @param price defini son tarif
+	 * @param dates defini la date du pass.
+	 * @param dayType une chaine de caractère decrivant le pass.
+	 */
 	public Pass(int type, int nombre, int price, ArrayList<LocalDate> dates, String dayType) {
 		super();
 		this.type = type;
@@ -45,38 +53,64 @@ public class Pass {
 	}
 
 
-
-
+	/**
+	 * methode accesor qui donne le nombre de pass dispoible
+	 * @return en entier quantité restante.
+	 */
 	public int getNombre() {
 		return nombre;
 	}
+	/**
+	 * Mutateur permettant de modifier le nombre de place disponible.
+	 *
+	 * @param nombre entier indquant la nouvelle quantité.
+	 */
 	public void setNombre(int nombre) {
 		this.nombre = nombre;
 	}
+	/**
+	 * methode accesor qui donne le type de pass.
+	 * @return en entier le type de pass
+	 */
 	public int gettype() {
 		return type;
 	}
-	public void settype(int type) {
-		this.type = type;
-	}
+	
+
+	
+	/**
+	 * Acceseur donne le prix du pass
+	 * @return un entier represente le prix.
+	 */
 	public int getPrice() {
 		return price;
 	}
+	
+	/**
+	 * Mutateur pour changement de prix d'un pass
+	 * @param price sera le nouveau prix en entier.
+	 */
 	public void setPrice(int price) {
 		this.price = price;
 	}
+	
+	/**
+	 * methode accesor donnant le ou les dates pour le pass.
+	 * @return un ArrayList de LocalDate avec un ou plusieurs date.
+	 */
 	public ArrayList<LocalDate> getDate() {
 		return dates;
 	}
-	public void setDate(ArrayList<LocalDate>  dates) {
-		this.dates = dates;
-	}
+	
+
+	/**
+	 * Accesseur donnant une chaine de caractere decrivant le pass
+	 * @return chaine de caractère avec le titre du pass.
+	 */
 	public String getDayType() {
 		return dayType;
 	}
-	public void setDayType(String dayType) {
-		this.dayType = dayType;
-	}
+
 	@Override
 	public String toString() {
 		return "Pass [type=" + type + ", nombre=" + nombre + ", price=" + price + "]";
