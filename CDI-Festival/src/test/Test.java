09a259967bd8616bfce065082c436feab44c7f32
Jonathan Fuentes <foujo@hotmail.com>
2016-11-24 16:37:28 +0100

    test ?

--- CDI-Festival/src/test/Test.java
+++ CDI-Festival/src/test/Test.java
@@ -1,12 +1,19 @@
 package test;
 
 import fr.cdiFestival.dao.article.RequestArticle;
+import fr.cdiFestival.model.Article;
 
 public class Test {
 public static void main(String[] args) {
 	
-	RequestArticle re = new RequestArticle();
-	System.out.println(re.getArticles());
+	Article a = new Article(112, "111", "23/05/78", "111", "11");
 	
+	RequestArticle re = new RequestArticle();
+//	System.out.println(re.add(a));
+//	System.out.println(re.getListArticle());
+	
+	System.out.println(re.upDate(a));
+	
+	System.out.println(re.getArticle(112));
 }
 }
