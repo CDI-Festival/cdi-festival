d9f534fc70ba502e47566a9bf1d60b49c7b0724f
Jonathan Fuentes <foujo@hotmail.com>
2016-11-24 11:44:01 +0100

    update (ArticleController / article.js / read.jsp / update.jsp)

--- CDI-Festival/WebContent/js/article.js
+++ CDI-Festival/WebContent/js/article.js
@@ -1,16 +1,25 @@
 /* Check empty area and change border in red if is it*/
-function isEmpty() {
-	author 	= document.querySelector('#author').value;
-	title 	= document.querySelector('#title').value;
-	content = document.querySelector('#content').value;
+	function isEmpty() {
+		author 	= document.querySelector('#author').value;
+		title 	= document.querySelector('#title').value;
+		content = document.querySelector('#content').value;
 
-	if (author == '') 	document.querySelector('#author').style.border 	= "2px solid red";
-	if (title == '') 	document.querySelector('#title').style.border 	= "2px solid red";
-	if (content == '') 	document.querySelector('#content').style.border = "2px solid red";
+		if (author == '') 	document.querySelector('#author').style.border 	= "2px solid red";
+		if (title == '') 	document.querySelector('#title').style.border 	= "2px solid red";
+		if (content == '') 	document.querySelector('#content').style.border = "2px solid red";
 	
-	else {
-		return true;
+		else {
+			return true;
+		}
+
+		return false;
 	}
 
+/*Warning message before deletion*/
+	function deleteConfirm() {
+		var ok=confirm("Êtes-vous sûr de vouloir supprimer l'article ?");
+	
+		if (ok) return true;	
+
 	return false;
-}
+	}
